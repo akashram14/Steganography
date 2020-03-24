b43ac8147cc62152ac63e6e4a42d161ec7e5a345
Akash <55029710+akashram14@users.noreply.github.com>
2020-03-24 14:24:39 +0530

    Update steg.java

--- new/src/GUI/steg.java
+++ new/src/GUI/steg.java
@@ -3,12 +3,15 @@ package GUI;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
 
 public class steg extends Application implements EventHandler<ActionEvent>{
 
@@ -24,6 +27,8 @@ public class steg extends Application implements EventHandler<ActionEvent>{
     public static void display(Stage stage){
         stage.setTitle("Title");
         Label label =new Label("Image Steganography");
+        label.setFont(Font.font ("Verdana", FontWeight.BOLD,20));
+
         Button button_encode = new Button();
         Button button_decode = new Button();
         button_encode.setText("Encode");
@@ -32,14 +37,14 @@ public class steg extends Application implements EventHandler<ActionEvent>{
         button_decode.setOnAction(e->decode_window.display(stage));
 
 
-
         VBox layout1 = new VBox(20);
+        layout1.setAlignment(Pos.CENTER);
         layout1.getChildren().addAll(label,button_decode,button_encode);
         main_scene=new Scene(layout1,300,250);
 
 
         stage.setScene(main_scene);
-        stage.setTitle("Hello");
+        stage.setTitle("Image Steganography");
         stage.show();
 
     }
